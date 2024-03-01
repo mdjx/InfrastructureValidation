5f9abf8aa7ab47db520a5db44347d99dfea44a7f
md <md@syslab.com.au>
2024-03-01 18:57:18 +1030

    Update Run.ps1 to modify sleep duration in test-loop from 5 to 10 seconds

--- Run.ps1
+++ Run.ps1
@@ -1,10 +1,6 @@
 ﻿param($TargetsFile = "targets.json")
 
-#[System.Console]::OutputEncoding = [System.Text.Encoding]::UTF8
 $HR = [System.Environment]::NewLine + "─"*100 + [System.Environment]::NewLine*2
 $PesterContainer = New-PesterContainer -Path "." -Data @{ TargetsFile=$TargetsFile}
 
-while ($true) {Get-Date; Write-Host ""; Invoke-Pester -Container $PesterContainer -Output Detailed; $HR; Start-Sleep -Seconds 5}
-
-
-
+while ($true) {Get-Date; Write-Host ""; Invoke-Pester -Container $PesterContainer -Output Detailed; $HR; Start-Sleep -Seconds 10}
