0594703a129673f5fcac866a3395c55595a4bd45
md <miloshd@aish.com.au>
2024-12-14 15:46:32 +1030

    added loop as argument option

--- Run.ps1
+++ Run.ps1
@@ -1,7 +1,15 @@
 ﻿#Requires -Modules @{ ModuleName="Pester"; ModuleVersion="5.0.0" }
-param($TargetsFile = "targets.json")
+param(
+    $TargetsFile = "targets.json",
+    [switch]$Loop
+)
 
 $HR = [System.Environment]::NewLine + "─"*100 + [System.Environment]::NewLine*2
 $PesterContainer = New-PesterContainer -Path "." -Data @{ TargetsFile=$TargetsFile}
 
-while ($true) {Get-Date; Write-Host ""; Invoke-Pester -Container $PesterContainer -Output Detailed; $HR; Start-Sleep -Seconds 10}
+if ($Loop) {
+    while ($true) {Get-Date; Write-Host ""; Invoke-Pester -Container $PesterContainer -Output Detailed; $HR; Start-Sleep -Seconds 10}
+}
+else {
+    Invoke-Pester -Container $PesterContainer -Output Detailed
+}
